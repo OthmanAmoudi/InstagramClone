1b92bea64a430640ec368d2f12da4bee44060c39
Othman Mashaab <red.fantastic@hotmail.com>
2017-04-19 14:20:43 +0300

    Initial Commit

---
+++ InstagramClone/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewController.swift
+//  InstagramClone
+//
+//  Created by Othman Mashaab on 19/04/2017.
+//  Copyright © 2017 Othman Mashaab. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InstagramClone/SignInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignInViewController.swift
+//  InstagramClone
+//
+//  Created by Othman Mashaab on 19/04/2017.
+//  Copyright © 2017 Othman Mashaab. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func dissmiss_OnClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
