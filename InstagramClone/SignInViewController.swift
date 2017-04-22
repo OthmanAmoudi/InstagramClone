2b4691946e324cb3a59841ddf56378db6d7ee7c7
Othman Mashaab <red.fantastic@hotmail.com>
2017-04-22 15:07:21 +0300

    finished the log in and the database image, also added button indicator in case of an empty textfields

--- InstagramClone/SignInViewController.swift
+++ InstagramClone/SignInViewController.swift
@@ -2,100 +2,60 @@
 //  SignInViewController.swift
 //  InstagramClone
 //
-//  Created by Othman Mashaab on 19/04/2017.
+//  Created by Othman Mashaab on 22/04/2017.
 //  Copyright © 2017 Othman Mashaab. All rights reserved.
 //
+
 import Foundation
 import UIKit
 import Firebase
 import FirebaseAuth
-import FirebaseStorage
+
 class SignInViewController: UIViewController {
 
-
-    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var profilePictureContainer: UIImageView!
-    
-    var selectedProfilePicture: UIImage?
-   
+    @IBOutlet weak var signInBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        profilePictureContainer?.layer.cornerRadius = 50
-       // profilePictureContainer?.layer.masksToBounds = true
-        profilePictureContainer?.clipsToBounds = true
-        let tap = UITapGestureRecognizer(target: self, action: #selector(SignInViewController.handleImageChosen) )
-        profilePictureContainer?.addGestureRecognizer(tap)
-        profilePictureContainer?.isUserInteractionEnabled = true
-        
 
-    }
-    
-    func handleImageChosen(){
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        present(picker, animated: true, completion: nil)
+        signInBtn.isEnabled=false
+        handleTextFields()
     }
 
-    @IBAction func signUpBtn_Touchupinside(_ sender: Any) {
-        FIRAuth.auth()?.createUser(withEmail: emailTextField.text!,password: passwordTextField.text!, completion: {(user: FIRUser?, error: Error?)in
-            if error != nil {
+
+    @IBAction func signInBtn_didClick(_ sender: Any) {
+        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: {(user, error) in
+            if error != nil{
                 print(error!.localizedDescription)
                 return
             }
-            let uid = user?.uid
-            let storageRef = FIRStorage.storage().reference(forURL: "gs://instagramclone-e03f1.appspot.com/").child("profile_image").child(uid!)
-            if let userPhotoimg =  self.selectedProfilePicture, let imageData = UIImageJPEGRepresentation(userPhotoimg, 0.1){
-                storageRef.put(imageData, metadata: nil, completion: {(metadata, error) in
-                 
-                    if error != nil{
-                        return
-                    }
-                    
-                    let profileImgUrl = metadata?.downloadURL()?.absoluteString
-                    let ref = FIRDatabase.database().reference()
-                    let userReference = ref.child("users")
-                    let newUserReference = userReference.child(uid!)
-                    newUserReference.setValue(["username": self.usernameTextField.text!, "email": self.emailTextField.text!, "Profile Picture":profileImgUrl])
-                    
-                })
-            }
-           
-     
+            self.performSegue(withIdentifier: "ToHomeTabBarSegue", sender: nil)
         })
         
     }
-    @IBAction func dissmiss_OnClick(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-
+ 
+override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
 }
 
-extension SignInViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
-        print("did finish picking")
-        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
-            profilePictureContainer.image = image
-            selectedProfilePicture = image
 
-        }
-        
-        print(info)
-      //  profilePictureContainer.image = infoPhoto
-        dismiss(animated: true, completion: nil)
+func handleTextFields(){
+    passwordTextField?.addTarget(self, action: #selector(SignInViewController.handleTextFieldDidChanged), for: UIControlEvents.editingChanged)
+    emailTextField?.addTarget(self, action: #selector(SignInViewController.handleTextFieldDidChanged), for: UIControlEvents.editingChanged)
+}
+
+func handleTextFieldDidChanged(){
+    guard let email = emailTextField.text, !email.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+        signInBtn.setTitleColor(UIColor.red, for: UIControlState.normal)
+        signInBtn.isEnabled=false
+        return
     }
     
-    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
-        dismiss(animated: true, completion: nil)
-        
-    }
-    
+    signInBtn.setTitleColor( .green , for: UIControlState.normal)
+    signInBtn.isEnabled=true
+}
+
+
 }
---
+++ InstagramClone/SignUpViewController.swift
@@ -0,0 +1,120 @@
+//
+//  SignInViewController.swift
+//  InstagramClone
+//
+//  Created by Othman Mashaab on 19/04/2017.
+//  Copyright © 2017 Othman Mashaab. All rights reserved.
+//
+import Foundation
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+class SignUpViewController: UIViewController {
+
+
+    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var profilePictureContainer: UIImageView!
+    
+    var selectedProfilePicture: UIImage?
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        profilePictureContainer?.layer.cornerRadius = 50
+       // profilePictureContainer?.layer.masksToBounds = true
+        profilePictureContainer?.clipsToBounds = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(SignUpViewController.handleImageChosen) )
+        profilePictureContainer?.addGestureRecognizer(tap)
+        profilePictureContainer?.isUserInteractionEnabled = true
+        signUpBtn?.isEnabled=false
+        handleTextFields()
+
+    }
+    
+    func handleTextFields(){
+        usernameTextField?.addTarget(self, action: #selector(SignUpViewController.handleTextFieldDidChanged), for: UIControlEvents.editingChanged)
+        passwordTextField?.addTarget(self, action: #selector(SignUpViewController.handleTextFieldDidChanged), for: UIControlEvents.editingChanged)
+        emailTextField?.addTarget(self, action: #selector(SignUpViewController.handleTextFieldDidChanged), for: UIControlEvents.editingChanged)
+    }
+    
+    func handleTextFieldDidChanged(){
+        guard let username = usernameTextField.text, !username.isEmpty, let email = emailTextField.text, !email.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+            signUpBtn.setTitleColor(UIColor.red, for: UIControlState.normal)
+            signUpBtn.isEnabled=false
+            return
+        }
+        
+        signUpBtn.setTitleColor( .green , for: UIControlState.normal)
+        signUpBtn.isEnabled=true
+    }
+    
+    func handleImageChosen(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+
+    @IBAction func signUpBtn_Touchupinside(_ sender: Any) {
+        FIRAuth.auth()?.createUser(withEmail: emailTextField.text!,password: passwordTextField.text!, completion: {(user: FIRUser?, error: Error?)in
+            if error != nil {
+                print(error!.localizedDescription)
+                return
+            }
+            let uid = user?.uid
+            let storageRef = FIRStorage.storage().reference(forURL: "gs://instagramclone-e03f1.appspot.com/").child("profile_image").child(uid!)
+            if let userPhotoimg =  self.selectedProfilePicture, let imageData = UIImageJPEGRepresentation(userPhotoimg, 0.1){
+                storageRef.put(imageData, metadata: nil, completion: {(metadata, error) in
+                 
+                    if error != nil{
+                        return
+                    }
+                    
+                    let profileImgUrl = metadata?.downloadURL()?.absoluteString
+                    let ref = FIRDatabase.database().reference()
+                    let userReference = ref.child("users")
+                    let newUserReference = userReference.child(uid!)
+                    newUserReference.setValue(["username": self.usernameTextField.text!, "email": self.emailTextField.text!, "Profile Picture":profileImgUrl])
+                    self.performSegue(withIdentifier: "SignUpToHomeTabBarSegue", sender: nil)
+                })
+            }
+           
+     
+        })
+        
+    }
+    @IBAction func dissmiss_OnClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
+extension SignUpViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        print("did finish picking")
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            profilePictureContainer.image = image
+            selectedProfilePicture = image
+
+        }
+        
+        print(info)
+      //  profilePictureContainer.image = infoPhoto
+        dismiss(animated: true, completion: nil)
+    }
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
